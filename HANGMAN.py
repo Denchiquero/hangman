9b8ee007528ab9ac51705adea5c129207c56d75e
Denchiquero <148644173+Denchiquero@users.noreply.github.com>
2024-01-05 22:10:21 +0300

    Update HANGMAN.py

--- HANGMAN.py
+++ HANGMAN.py
@@ -1,19 +1,28 @@
 import random
+import os
+
+file_txt = open('C:\\Program Files (x86)\\Hangman\\dict.txt', 'r', encoding="utf-8")
+dictionary = file_txt.read().split()
 
 rus_lan = ['Русский', 'русский', 'Heccrbq', 'heccrbq']
 eng_lan = ['English', 'english', 'Утпдшыр', 'утпдшыр']
+
+wordlist_1 = []
+wordlist_2 = []
+wordlist_3 = []
 
 rus_dict = {'welcome_word': """Привет! Добро пожаловать в игру Виселица!
 Правила просты: я загадал слово, а ты должен его угадать.
 Но учти, что за каждой ошибкой следует наказание!
 У тебя будет 9 попыток. По истечении их запаса тебя повесят) 
 Желаю тебе удачи!""",
-            'wordlist_1': ['кот', 'чип', 'чай', 'руль', 'окно', 'небо', 'ухо'],
-            'wordlist_2': ['чашка', 'лодка', 'мышка', 'класс', 'ручка', 'доска', 'кактус'],
-            'wordlist_3': ['компьютер', 'итератор', 'калькулятор', 'видеокарта', 'процессор', 'программа', 'монитор'],
+            'wordlist_1': wordlist_1,
+            'wordlist_2': wordlist_2,
+            'wordlist_3': wordlist_3,
             'choose_lev': 'Выбери уровень игры: 1, 2, 3',
             'hello_again': ['Что ж, еще разок!', 'Заново!', 'На спавн!', 'Ах щит! Хи ви го эгейн!'],
             'not_guess': ['Неее', 'Неверно', 'А подумать?'],
+            'guessed': ['Правильно!', 'Так держать!', 'Молодец!', 'Ты угадал!', 'В точку!'],
             'happy': 'О! Рад видеть тебя снова!',
             'you_won': '\nТы выиграл!',
             'call_a_letter': '\n\nНазови букву: ',
@@ -21,7 +30,8 @@ rus_dict = {'welcome_word': """Привет! Добро пожаловать в 
             'used': 'Использованные буквы: ',
             'game_over': '\n\nТы проиграл! Это слово: ',
             'one_more': 'Сыграем еще раз?(да/нет)',
-            'again': ['да', 'Да', 'ДА', 'if', 'If', 'IF']
+            'again': ['да', 'Да', 'ДА', 'if', 'If', 'IF'],
+            'already_used': 'Вы уже использовали эту букву. Попробуйте другую'
             }
 
 eng_dict = {'welcome_word': """Hello! Welcome to Hangman game!
@@ -36,7 +46,8 @@ Good luck!""",
                            'doorbell', 'grandfather', 'relaxation'],
             'choose_lev': 'Choose game level: 1, 2, 3',
             'hello_again': ['Well, one more time!', 'Again!', 'To the spawn!', 'Oh shit! Here we go again!'],
-            'not_guess': ['Nooo', 'Wrong', 'How about thinking?'],
+            'not_guessed': ['Nooo', 'Wrong', 'How about thinking?'],
+            'guessed': ['Right!', 'Keep it up!', 'Well done!', 'You guessed!', 'Exactly!'],
             'happy': 'Wow! glad to see you again!',
             'you_won': '\nYou win!',
             'call_a_letter': '\n\nName a letter: ',
@@ -44,8 +55,20 @@ Good luck!""",
             'used': 'Used letters: ',
             'game_over': '\n\nGame over! The word is: ',
             'one_more': 'Do you wanna play one more time?(yes/no)',
-            'again': ['yes', 'Yes', 'YES', 'нуы', 'Нуы', 'НУЫ']
+            'again': ['yes', 'Yes', 'YES', 'нуы', 'Нуы', 'НУЫ'],
+            'already_used': 'You have already used this letter. Try another one'
             }
+
+
+def sortirovka():
+    for i in dictionary:
+        match len(i):
+            case 3|4|5:
+                wordlist_1.append(i)
+            case 6|7|8:
+                wordlist_2.append(i)
+            case 9|10|11|12|13|14|15|16|17|18:
+                wordlist_3.append(i)
 
 
 def language():
@@ -70,6 +93,7 @@ def selected_language(lan):
 
 
 def level(lev):
+    sortirovka()
     if lev == '1':
         return dict_lan['wordlist_1']
     if lev == '2':
@@ -86,9 +110,32 @@ def hang():
 / \\""")
 
 
+def man():
+    global word, turns
+    match turns:
+        case 8:
+            print('\n | ')
+        case 7:
+            print(' |\n\U0001F642 ')
+        case 6:
+            print(' |\n\U0001F610 \n |')
+        case 5:
+            print(' |\n\U0001F62C \n | \n |')
+        case 4:
+            print(' |\n\U0001F61E \n | \n | \n/')
+        case 3:
+            print(' |\n\U0001F974 \n | \n | \n/ \\')
+        case 2:
+            print(' |\n\U0001F628 \n/| \n | \n/ \\')
+        case 1:
+            print(' |\n\U0001F628 \n/|\\ \n | \n/ \\')
+        case 0:
+            hang()
+            print(f'{dict_lan['game_over']}', word)
+
+
 def hangman():
-    global games
-    global answer
+    global games, answer, turns, word
     games += 1
     if games == 2:
         print(f'{dict_lan['happy']}')
@@ -96,14 +143,14 @@ def hangman():
         print(f'{random.choice(dict_lan['hello_again'])}')
     lev = input(f'{dict_lan['choose_lev']}\n')
     wordlist = level(lev)
+    word = random.choice(wordlist)
     alphabet = []
-    secret = random.choice(wordlist)
     guesses = ''
-
+    os.system('cls||clear')
     turns = 9
     while turns > 0:
         missed = 0
-        for letter in secret:
+        for letter in word:
             if letter in guesses:
                 print(letter, end=' ')
             else:
@@ -113,35 +160,26 @@ def hangman():
             print(f'{dict_lan['you_won']}')
             break
         guess = input(f'{dict_lan['call_a_letter']}')
-        alphabet.append(guess)
-        guesses += guess
-        if guess not in secret:
-            turns -= 1
-            print(f'\n{random.choice(dict_lan['not_guess'])}')
+        os.system('cls||clear')
+        if guess not in guesses:
+            guesses += guess
+            alphabet.append(guess)
+            if guess not in word:
+                turns -= 1
+                print(f'{random.choice(dict_lan['not_guess'])}')
+                print(f'{dict_lan['turns_left']}', turns)
+                print(f'{dict_lan['used']}', ' '.join(alphabet))
+                man()
+            else:
+                print(f'{random.choice(dict_lan['guessed'])}')
+                print(f'{dict_lan['turns_left']}', turns)
+                print(f'{dict_lan['used']}', ' '.join(alphabet))
+                man()
+        else:
+            print(f'{dict_lan['already_used']}')
             print(f'{dict_lan['turns_left']}', turns)
             print(f'{dict_lan['used']}', ' '.join(alphabet))
-            match turns:
-                case 8:
-                    print('\n | ')
-                case 7:
-                    print(' |\n\U0001F642 ')
-                case 6:
-                    print(' |\n\U0001F610 \n |')
-                case 5:
-                    print(' |\n\U0001F62C \n | \n |')
-                case 4:
-                    print(' |\n\U0001F61E \n | \n | \n/')
-                case 3:
-                    print(' |\n\U0001F974 \n | \n | \n/ \\')
-                case 2:
-                    print(' |\n\U0001F628 \n/| \n | \n/ \\')
-                case 1:
-                    print(' |\n\U0001F628 \n/|\\ \n | \n/ \\')
-                case 0:
-                    hang()
-                    print(f'{dict_lan['game_over']}', secret)
-        else:
-            print(f'{dict_lan['used']}', ' '.join(alphabet))
+            man()
 
     answer = input(f'{dict_lan['one_more']}')
 
@@ -149,11 +187,12 @@ def hangman():
 answer = ''
 games = 0
 dict_lan = {}
+word = ''
+turns = 0
 
 
 def main():
-    print("""HANGMAN.PY ver. 1.4
-    \U000000A9 Denis Krutov. All rights reversed""")
+    print('HANGMAN.PY ver. 1.6 \n\U000000A9 Denis Krutov. All rights reversed')
 
     language()
 
